3c11c232de0e0f1c4c5ba7c96de71be5b9500766
Damon <m0956587266@gmail.com>
2023-05-03 13:33:22 +0800

    deleted:    1121-PHP-login/check.php

--- 1121-PHP-login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-echo "己登出使用者".$_SESSION['name'];
-
-unset($_SESSION['name']);
--- 1121-PHP-login/name.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//echo $_COOKIE['name'];
-session_start();
-
-echo $_SESSION['name'];
-?>
